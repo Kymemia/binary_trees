e21623728e0cf7d524f3cd6d22346931fa897d46
Kymemia <kimemiachristopherr@gmail.com>
2024-03-27 18:48:38 +0300

    2. Insert right *

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -20,7 +20,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new_node->left = NULL;
 	new_node->right = NULL;
 
-	if (parent->left != NULL)
+	if (parent->right != NULL)
 	{
 		new_node->right = parent->right;
 		new_node->right->parent = new_node;
