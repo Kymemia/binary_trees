a163248b6a919ce5182cbde994c010aefa913014
Kymemia <kimemiachristopherr@gmail.com>
2024-03-28 01:41:49 +0300

    12. Leaves *

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,14 +8,14 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	int left_leaves;
+	int right_leaves;
+
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-
-	int left_leaves;
-	int right_leaves;
 
 	left_leaves = binary_tree_leaves(tree->left);
 	right_leaves = binary_tree_leaves(tree->right);
