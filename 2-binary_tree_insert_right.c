ce59103f01d7843831f12cd610b9de3649885f39
Kymemia <kimemiachristopherr@gmail.com>
2024-03-29 14:52:01 +0300

    2-loopfix

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,10 +12,13 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	if (parent == NULL || new_node == NULL)
+	if (parent == NULL)
 		return (NULL);
 
 	new_node = malloc(sizeof(binary_tree_t));
+
+	if (new_node == NULL)
+		return (NULL);
 
 	new_node->n = value;
 	new_node->parent = parent;
